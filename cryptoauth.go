b8155756807748b08d1570beade03a13bc00566d
JPH <jph@hackworth.be>
2015-01-26 05:14:05 +0000

    added full handshake.challenge decoding via gopacket

--- cryptoauth.go
+++ cryptoauth.go
@@ -233,8 +233,8 @@ type Handshake struct {
 	layers.BaseLayer
 	Stage uint32
 	//Challenge     CryptoAuth_Challenge
-	Challenge     [12]byte  // We use a generic container initially then decode it into appropriate struct later
-	Nonce         [24]uint8 // 24 bytes
+	Challenge     CryptoAuth_Challenge // We use a generic container initially then decode it into appropriate struct later
+	Nonce         [24]uint8            // 24 bytes
 	PublicKey     [32]uint8
 	Authenticator [16]uint8 // 16 bytes
 	TempPublicKey [32]uint8 // 32 bytes
@@ -254,6 +254,7 @@ func (h *Handshake) LayerType() gopacket.LayerType {
 func decodeHandshake(data []byte, p gopacket.PacketBuilder) error {
 
 	h := &Handshake{}
+	//h := new(CryptoAuth_Handshake)
 	err := h.DecodeFromBytes(data, p)
 	if err != nil {
 		return err
@@ -278,7 +279,10 @@ func (h *Handshake) DecodeFromBytes(data []byte, df gopacket.DecodeFeedback) err
 		log.Println("boobs")
 	}
 	log.Println("bigendian stage ", h.Stage)
-	binary.Read(r, binary.BigEndian, &h.Challenge)
+	binary.Read(r, binary.BigEndian, &h.Challenge.Type)
+	binary.Read(r, binary.BigEndian, &h.Challenge.Lookup)
+	binary.Read(r, binary.BigEndian, &h.Challenge.RequirePacketAuthAndDerivationCount)
+	binary.Read(r, binary.BigEndian, &h.Challenge.Additional)
 	binary.Read(r, binary.BigEndian, &h.Nonce)
 	binary.Read(r, binary.BigEndian, &h.PublicKey)
 	binary.Read(r, binary.BigEndian, &h.Authenticator)
