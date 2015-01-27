38915524d17a8d6f4677e7fce3cdf87d3805a704
JPH <jph@hackworth.be>
2015-01-27 11:23:15 +0000

    removed pcap.go, cleaning up types.go and util.go

--- types.go
+++ types.go
@@ -1,12 +1,9 @@
 package main
 
 import (
-	"code.google.com/p/gopacket"
-	"code.google.com/p/gopacket/layers"
 	"github.com/nsjph/tun"
 	"github.com/op/go-logging"
 	"net"
-	"time"
 )
 
 type ServerInfo struct {
@@ -24,65 +21,6 @@ type PeerInfo struct {
 	PublicKey     []byte
 	SharedKey     []byte
 }
-
-// type CryptoAuth struct {
-// }
-
-// type TCPHeader struct {
-// 	Source      uint16
-// 	Destination uint16
-// 	SeqNum      uint32
-// 	AckNum      uint32
-// 	DataOffset  uint8 // 4 bits
-// 	Reserved    uint8 // 3 bits
-// 	ECN         uint8 // 3 bits
-// 	Ctrl        uint8 // 6 bits
-// 	Window      uint16
-// 	Checksum    uint16 // Kernel will set this if it's 0
-// 	Urgent      uint16
-// 	Options     []TCPOption
-// }
-
-// type CryptoAuthHeader struct {
-// 	State         uint8
-// 	Challenge     [3]byte
-// 	Nonce         [6]byte
-// 	PermPublicKey [8]byte
-// 	Authenticator [4]byte
-// 	TempPublicKey [8]byte
-// 	Content       []byte
-// }
-
-// type CryptoAuthHeader struct {
-// 	State         uint8
-// 	Challenge     [3]uint8
-// 	Nonce         [6]uint8
-// 	PermPublicKey [8]uint8
-// 	Authenticator [4]uint8 // shared secret?
-// 	TempPublicKey [8]uint8
-// }
-
-// type CryptoAuthPacket struct {
-// 	Header CryptoAuthHeader
-// }
-
-// type CryptoAuthChallengeHeader struct {
-// 	Type   uint8
-// 	Lookup [7]uint8
-// }
-
-// type CryptoAuthType struct {
-// }
-
-// type CryptoAuthHeader1 struct {
-// 	State         uint32
-// 	Challenge     uint64
-// 	Nonce         [24]uint8
-// 	PermPublicKey [32]uint8
-// 	Authenticator [16]uint8
-// 	TempPublicKey [32]uint8
-// 	Content       []byte
-// }
 
 type TomlConfig struct {
 	Server ServerConfig
@@ -117,11 +55,6 @@ type InterfaceController struct {
 	ifaces []*UDPInterface
 }
 
-// type Interface interface {
-// 	Read(b []byte) (n int, err error)
-// 	Start(ic *InterfaceController, c ServerConfig)
-// }
-
 type Router struct {
 	Iface      *tun.Tun
 	PublicKey  [32]byte
@@ -149,13 +82,6 @@ type KeyPair struct {
 	publicKey  [32]byte
 	privateKey [32]byte
 }
-
-// type PeerKeys struct {
-// 	publicKey    [32]byte
-// 	sharedSecret [32]byte
-// 	password     []byte
-// 	tempKeyPair  *KeyPair
-// }
 
 type Peer struct {
 	addr *net.UDPAddr
@@ -186,91 +112,4 @@ type Message struct {
 type EncryptedMessage2 struct {
 	handshake [120]byte
 	payload   []byte
-}
-
-type PeerName string
-
-type UDPPacket struct {
-	Name   PeerName
-	Packet []byte
-	Sender *net.UDPAddr
-}
-
-type UDPSender interface {
-	Send([]byte) error
-	Shutdown() error
-}
-
-type Connection interface {
-	Local() *Peer
-	Remote() *Peer
-	RemoteTCPAddr() string
-	Established() bool
-	Shutdown()
-}
-
-type ForwardedFrame struct {
-	srcPeer *Peer
-	dstPeer *Peer
-	frame   []byte
-}
-
-// type Peer struct {
-// 	sync.RWMutex
-// 	Name          PeerName
-// 	NameByte      []byte
-// 	UID           uint64
-// 	version       uint64
-// 	localRefCount uint64
-// 	connections   map[PeerName]Connection
-// }
-
-type RemoteConnection struct {
-	local         *Peer
-	remote        *Peer
-	remoteTCPAddr string
-}
-
-type LocalConnection struct {
-	remoteUDPAddr *net.UDPAddr
-	established   bool
-	stackFrag     bool
-	effectivePMTU int
-	SessionKey    *[32]byte
-	heartbeat     *time.Ticker
-	fetchAll      *time.Ticker
-	fragTest      *time.Ticker
-	forwardChan   chan<- *ForwardedFrame
-	forwardChanDF chan<- *ForwardedFrame
-	stopForward   chan<- interface{}
-	stopForwardDF chan<- interface{}
-	verifyPMTU    chan<- int
-	//Decryptor     Decryptor
-	Router   *Router
-	UID      uint64
-	shutdown bool
-	//queryChan chan<- *ConnectionInteraction
-}
-
-type SimpleUDPSender struct {
-	conn    *LocalConnection
-	udpConn *net.UDPConn
-}
-type RawUDPSender struct {
-	ipBuf     gopacket.SerializeBuffer
-	opts      gopacket.SerializeOptions
-	udpHeader *layers.UDP
-	socket    *net.IPConn
-	conn      *LocalConnection
-}
-
-type PacketSource interface {
-	ReadPacket() ([]byte, error)
-}
-type PacketSink interface {
-	WritePacket([]byte) error
-}
-type PacketSourceSink interface {
-	PacketSource
-	PacketSink
 }
